45fd1110cd729bee138146a49e41eab5cd6e6bbf
onurabdulaji <onurabdulaji@gmail.com>
2025-06-02 10:32:55 +0200

    JQuery , Main Layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   standalone: false,
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected title = 'ETicaretClient';
 }
+
+$(document).ready(() => {
+  alert('Document is ready!');
+});
